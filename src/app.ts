f270e659d48d4366d20b4d9d7b86def8533429e6
romeucajamba <romeucajamba@gmail.com>
2024-10-21 16:14:42 +0100

    chore: romove importacoes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { join } from 'node:path';
 import path from 'path';
 import express from 'express';
 
 export const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "public")))
-
-//const __dirname = dirname(fileURLToPath(import.meta.url));
-//const __dirname = path.resolve();
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
